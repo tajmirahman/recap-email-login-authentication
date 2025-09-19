00193b69c1eb311a25e208bcf1f85cb35fb228c1
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-09-19 21:54:32 +0600

    recap email login authentication

---
+++ my-project/src/Components/MainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div >
+            <h1 className='text-center'>This is main layout page</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
