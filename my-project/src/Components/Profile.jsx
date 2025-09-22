26b87212e3c9f407d03278714322490780ea288b
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-09-22 20:07:13 +0600

    sign out option done successfully

--- my-project/src/Components/Profile.jsx
+++ my-project/src/Components/Profile.jsx
@@ -1,13 +1,29 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import auth from '../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
+
+    const navigate=useNavigate();
+
+    const handleSignOut=()=>{
+        // console.log('yes')
+        signOut(auth)
+        .then(()=>{
+            console.log('account success fully logout');
+            navigate('/');
+        })
+        .catch((err)=>{console.log(err)})
+    }
+
     return (
         <div className='flex justify-between items-center w-9/12 mx-auto mt-10'>
             <div className=''>
                 <h1 className='text-2xl text-center'>Wellcome to Dashboard</h1>
             </div>
             <div className='text-end'>
-                <button className='btn bg-red-300'>SignOut</button>
+                <button onClick={handleSignOut} className='btn bg-red-300'>SignOut</button>
             </div>
         </div>
     );
